b81a0727e1692f07a7bab1d5c80de77d616d2157
YASTREAMER <yasharya482190@gmail.com>
2024-05-29 16:11:07 +0530

    Added the main file and Dataloader file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import os 
+import pandas as pd 
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -17,5 +16,43 @@ import torch.optim.lr_scheduler as lr_scheduler
 
 from tqdm.notebook import tqdm
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-device
+import cv2
+
+from DataLoader import dataloader
+
+class Main:
+
+    def __init__(self) -> None:
+
+        self.dataloader = dataloader()
+
+        #Setting the size of the training dataset
+        self.datasize=64
+
+        #Checking if cuda is available or not 
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        #List for storing the training dataset
+        self.imagestrain=[]
+        self.labeltrain=[]
+
+        #Init the list to store pixel value of each image
+        self.pixelvalue=[]
+
+    def MainTrain(self) -> None:
+
+        #Loading the training dataset
+        self.imagestrain=self.dataloader.GetImages(dir="Train/low/")
+        self.labeltrain=self.dataloader.GetImages(dir="Train/high/")
+
+        for i in range(self.datasize):
+            self.pixelvalue.append(self.dataloader.GetPixelValues(self.imagestrain[i]))
+            
+
+
+
+if __name__ == "__main__":
+
+    main = Main()
+    main.MainTrain()
+
